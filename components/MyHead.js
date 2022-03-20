a4e688d9c743c0dae2d9eb40642078a4a27f39f7
Maluki Muthusi <malukimuthusi@gmail.com>
2022-03-21 01:15:15 +0300

    Add App bar

--- components/MyHead.js
+++ components/MyHead.js
@@ -1,11 +1,14 @@
-import Head from 'next/head';
+import Head from "next/head"
 
 function MyHead(props) {
   return (
     <Head>
-      <meta charset='utf-8'></meta>
-      <meta name='viewport' content='initial-scale=1, width=device-width' />
+      <meta charset="utf-8"></meta>
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+      <link rel="icon" href="/favicon/favicon.ico" />
       <title>The Alumni Challenge</title>
     </Head>
-  );
+  )
 }
+
+export default MyHead
--- components/MyNavigationBar.js
+++ components/MyNavigationBar.js
@@ -1,57 +1,63 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import Logo from '../public/logo.png';
+import * as React from "react"
+import Image from "next/image"
+import Link from "next/link"
+import Logo from "../public/logo.png"
+import AppBar from "@mui/material/AppBar"
+import styles from "../styles/components/MyNavigationBar.module.css"
+import PropTypes from "prop-types"
+import useScrollTrigger from "@mui/material/useScrollTrigger"
 
-export function MyNavigationBar(props) {
+export default function MyNavigationBar(props) {
   return (
-    <nav>
-      <div>
-        <Image src={Logo} width={400} height={400} alt={'Company logo'} />
-        <div>
+    <nav className={styles.nav}>
+      <ElevationScroll {...props}>
+        <AppBar className={styles.appBar}>
+          <Image src={Logo} width={40} height={40} alt={"Company logo"} />
+
           <div>
-            <Link href={'/'}>
+            <Link href={"/"}>
               <a>Home</a>
             </Link>
           </div>
 
           <div>
-            <Link href={'/organizers'}>
+            <Link href={"/organizers"}>
               <a>Organizers</a>
             </Link>
           </div>
 
           <div>
-            <Link href={'/partners'}>
+            <Link href={"/partners"}>
               <a>Partners</a>
             </Link>
           </div>
 
           <div>
-            <Link href={'/contact-us'}>
+            <Link href={"/contact-us"}>
               <a>Contact Us</a>
             </Link>
           </div>
-        </div>
-      </div>
+        </AppBar>
+      </ElevationScroll>
     </nav>
-  );
+  )
 }
 
 function ElevationScroll(props) {
-  const { children } = props;
+  const { children } = props
   // Note that you normally won't need to set the window ref as useScrollTrigger
   // will default to window.
   // This is only being set here because the demo is in an iframe.
   const trigger = useScrollTrigger({
     disableHysteresis: true,
     threshold: 0,
-  });
+  })
 
   return React.cloneElement(children, {
     elevation: trigger ? 4 : 0,
-  });
+  })
 }
 
 ElevationScroll.propTypes = {
   children: PropTypes.element.isRequired,
-};
+}
