44c8725a5f084f573e461062393efd7f26182b99
Maluki Muthusi <malukimuthusi@gmail.com>
2022-03-21 23:20:08 +0300

    Add mission section

--- components/About.js
+++ components/About.js
@@ -1,9 +1,33 @@
-import { Typography } from "@mui/material"
+import { Paper, Typography } from "@mui/material"
+import styles from "../styles/components/About.module.css"
 
 export default function About(props) {
   return (
-    <div>
-      <Typography variant="h6">About the Alumni Challenge</Typography>
+    <div className={styles.about}>
+      <Paper elevation={0}>
+        <Typography variant="h5" component="h2" className={styles.title}>
+          About the Alumni Challenge
+        </Typography>
+
+        <Typography variant="body1" component="p" className={styles.body}>
+          Alumni Challenge is an initiative to drive back Alumni participation
+          in tangible development in their former school, college or university
+          through different sports and social avenues.
+        </Typography>
+
+        <Typography variant="body1" component="p">
+          Alumni Golf Challenge is a golf Charity tournament that focuses on
+          attracting corporate executives and business men and women to
+          fundraise education bursaries for their former school, college or
+          university institution.
+        </Typography>
+
+        <Typography variant="body1" component="p">
+          Company executives and business leaders will battle it out in the golf
+          challenge and the winners walks away with Bursary fund to support
+          vulnerable students in their alma mater
+        </Typography>
+      </Paper>
     </div>
   )
 }
--- components/Mission.js
+++ components/Mission.js
@@ -1,9 +1,30 @@
 import { Typography } from "@mui/material"
+import Image from "next/image"
+import childBooks from "../public/child-books.png"
 
 export default function Mission(props) {
   return (
     <div>
-      <Typography variant="h6">Objectives</Typography>
+      <Typography variant="h6">Mission</Typography>
+      <Image
+        src={childBooks}
+        height={1024}
+        width={790}
+        alt="Child with books"
+      />
+      <Typography variant="body1" component="p">
+        Encourage tangible impact on alma mater through bursary support
+      </Typography>
+
+      <Typography variant="body1" component="p">
+        Encourage Outreach programmes targeting learners through sports and
+        other CSR efforts
+      </Typography>
+
+      <Typography variant="body1" component="p">
+        Networking opportunities among corporates that ride on Alumni
+        associations
+      </Typography>
     </div>
   )
 }
