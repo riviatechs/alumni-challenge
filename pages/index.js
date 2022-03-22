4a968e1ea9e1686a77aa1011d9ecc812105d170f
Frank Bosire <frankbosire2017@gmail.com>
2022-03-22 15:44:08 +0300

    styles changes

--- pages/index.js
+++ pages/index.js
@@ -14,11 +14,8 @@ import Mission from "../components/Mission"
 import Footer from "../components/Footer"
 import Organizer from "../components/Organizer"
 import Previous from "../components/Previous"
-<<<<<<< HEAD
 import { Fragment } from "react"
-=======
 import styles from "../styles/Home.module.css"
->>>>>>> 31d4a5cca82fdbd38411fed9bb662ab396bf89b1
 
 export default function Home() {
   return (
