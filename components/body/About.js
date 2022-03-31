17b5c59e1982657cf8cc4619e7f7698b0cdd6872
Frank Bosire <frankbosire2017@gmail.com>
2022-03-31 09:11:33 +0300

    added mission and vision

--- components/body/About.js
+++ components/body/About.js
@@ -3,6 +3,8 @@ import { Paper, Typography } from "@mui/material"
 
 import styles from "../../styles/components/About.module.css"
 import Golf from "../../public/golf.png"
+import missionImage from "../../public/about/mission.png"
+import visionImage from "../../public/about/vision.png"
 import Play from "../../public/play.jpg"
 
 const golfText = "Golf club and a flag"
@@ -13,6 +15,33 @@ export default function About(props) {
     <div className={styles.about}>
       <h1 className={styles.title}>About the Alumni Challenge</h1>
 
+      <div className={styles.visionMission}>
+        <div className={styles.mission}>
+          <div>
+            <Image
+              src={missionImage}
+              width={620}
+              height={403}
+              alt="mission image"
+            />
+          </div>
+          <h1>Mission</h1>
+          <div>content</div>
+        </div>
+        <div className={styles.vision}>
+          <div>
+            <Image
+              src={visionImage}
+              width={522}
+              height={478}
+              alt="mission image"
+            />
+          </div>
+          <h1>Vision</h1>
+          <div>content</div>
+        </div>
+      </div>
+
       <div className={styles.body}>
         <section className={styles.section}>
           <p className={styles.content}>
@@ -20,24 +49,16 @@ export default function About(props) {
             in tangible development in their former school, college or
             university through different sports and social avenues.
           </p>
-
-          <Image
-            src={Golf}
-            width={477}
-            height={465}
-            alt={playGolf}
-            className={styles.image}
-          />
+          <div className={styles.image}>
+            <Image src={Golf} width={477} height={465} alt={playGolf} />
+          </div>
         </section>
 
         <section className={styles.section}>
-          <Image
-            src={Play}
-            width={477}
-            height={465}
-            alt={golfText}
-            className={styles.image}
-          />
+          <div className={styles.image}>
+            <Image src={Play} width={477} height={465} alt={golfText} />
+          </div>
+
           <p className={styles.content}>
             Alumni Golf Challenge is a golf Charity tournament that focuses on
             attracting corporate executives and business men and women to
@@ -46,11 +67,11 @@ export default function About(props) {
           </p>
         </section>
 
-        <p className={styles.content}>
+        {/* <p className={styles.content}>
           Company executives and business leaders will battle it out in the golf
           challenge and the winners walks away with Bursary fund to support
           vulnerable students in their alma mater
-        </p>
+        </p> */}
       </div>
     </div>
   )
